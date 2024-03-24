394173548d506281f024f5f29100ca6d61359cdc
epass1123 <404.notfound@kakao.com>
2024-03-24 15:33:09 +0900

    feat: add user view

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,4 +1,81 @@
 package controller;
 
-public class UserController {
+import model.Member;
+import model.Trainer;
+import model.User;
+import repository.MemerRepository;
+import view.UserView;
+
+import java.util.Scanner;
+enum Role{
+    TRAINER, MEMBER, USER, ADMIN
 }
+class MenuException extends Exception{
+    MenuException(){
+
+    }
+
+    MenuException(String message){
+        super(message);
+    }
+}
+public class UserController {
+    UserView view = new UserView();
+    //GroupPTRepository repo = new GroupPTRepository();
+    Scanner sc = new Scanner(System.in);
+    User user;
+    Trainer trainer;
+    Member member;
+    void signUp(){
+        view.showSignupMenu();
+        view.requestName();
+        String name = sc.nextLine();
+        view.requestAge();
+        String age = sc.nextLine();
+        view.requestSex();
+        String sex = sc.nextLine();
+        view.requestId();
+        String id = sc.nextLine();
+        view.requestPw();
+        String pw = sc.nextLine();
+        view.requestPhoneNumber();
+        String phoneNumber = sc.nextLine();
+        view.requestRole();
+        Role role = Role.valueOf(sc.nextLine());
+        user = new User(name,age,sex,id,pw,phoneNumber);
+        if(role == Role.TRAINER){
+//            trainer = new Trainer(user);
+        }
+        else if (role == Role.MEMBER){
+//            member = new Member(user);
+        }
+//        repo.saveMember(member);
+//        repo.saveTrainer(trainer);
+        view.showSigned();
+    }
+
+    void consult(){
+
+    }
+
+    void reserveConsultation(){
+
+    }
+
+    void chooseAvailableTime(){
+
+    }
+
+    void checkMyReservation(){
+
+    }
+
+    void changeReservation(){
+
+    }
+
+    void cancelReservation(){
+
+    }
+}
+
--- src/view/UserView.java
+++ src/view/UserView.java
@@ -1,4 +1,46 @@
 package view;
 
+import java.util.Scanner;
+
 public class UserView {
+    public void showSignupMenu() {
+        System.out.println("*************************************************");
+        System.out.println("형식에 맞는 양식을 작성해 주세요.");
+        System.out.println("*************************************************");
+    }
+
+    public void requestName() {
+        System.out.print("이름: ");
+    }
+
+    public void requestAge() {
+        System.out.print("나이: ");
+    }
+
+    public void requestSex() {
+        System.out.print("성별: ");
+    }
+
+    public void requestId() {
+        System.out.print("아이디: ");
+    }
+
+    public void requestPw() {
+        System.out.print("비밀번호: ");
+    }
+
+    public void requestPhoneNumber() {
+        System.out.print("휴대폰번호: ");
+    }
+
+    public void requestRole() {
+        System.out.print("뭘로가입? : ");
+    }
+
+    public void showSigned() {
+        System.out.println("*************************************************");
+        System.out.println("회원가입 신청이 완료되었습니다. 승인 메시지가 전송되면 로그인이 가능합니다.");
+        System.out.println("*************************************************");
+
+    }
 }
