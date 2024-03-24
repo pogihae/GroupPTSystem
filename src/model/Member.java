5f1ccaa0f92d7ef6ef4600934441ab255fb38a5b
hhyunjju <hhunjju0618@gmail.com>
2024-03-24 16:39:16 +0900

    reserveClass() -ing

--- src/model/Member.java
+++ src/model/Member.java
@@ -1,6 +1,9 @@
 package model;
 
+import repository.GroupPTRepository;
+
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Scanner;
 
 public class Member extends User{
@@ -42,13 +45,49 @@ public class Member extends User{
         }
         this.payment = new Payment(LocalDate.now(), this.getPhoneNumber(),this.selectedOption);
         System.out.println(choice+"번 옵션, "+selectedOption.getPrice()+"원 결제가 완료되었습니다");
-
+        this.remainSessionCount += selectedOption.getSessions();
         //payment 객체를 "결제" 파일에 저장하는 작업
+        //노쇼 1회 시 재등록(결제) 불가능하다
     }
     //수업예약
+    public void reserveClass(){
+        GroupPTRepository repository = new GroupPTRepository();
+        //남은회수 있는지 확인 후 => 있으면
+        //1. 트레이너 선택메뉴 => 선택
+        //2. 해당 트레이너의 수업 예약 가능한 시간 목록 출력 => 선택
+        //3. 예약 확정 => 예약확정 메세지 출력
+        if(selectedOption.getSessions()==0){
+            System.out.println("남은 횟수가 없습니다. 재결제가 필요합니다");
+            return;
+        }
+        //1. 트레이너 선택 메뉴(이름, 등급, 성별)
+        List<Trainer> allTrainersList = repository.findAllTrainers();
+        for(int i=0; i<allTrainersList.size();i++) {
+            Trainer trainer = allTrainersList.get(i);
+            System.out.printf("%d. %s 트레이너/ 등급 %s/ %s", i+1, trainer.getName(), trainer.getGrade()
+            , trainer.getSex());
+            System.out.println();
+        }
 
+
+
+
+
+        /*
+        1. 원하는 시간이 없으면 ‘0’번을 눌러 다시 트레이너 목록이 보여진다.
+        2. ‘*’을 누르면 메인화면으로 돌아간다.
+        3. 선택지에 없는 메뉴를 선택할 경우 메뉴를 다시 출력한다.
+         */
+    }
     //수업정보확인
+    //(수업 예약 확인, PT 선생님, 노쇼, 남은 수업 횟수,남은 수업 사용 가능 일수)
     public void displayReservationInfo(){
 
     }
+
+    //수업예약취소
+    //당일취소할 경우, 취소는 되지만 횟수가 차감된다
+
+    //수업예약변경
+    //당일예약변경 불가능하다
 }
