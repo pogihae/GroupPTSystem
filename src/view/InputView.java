d0c5bc9e0d47432f464aa451c8ed01d6c37bf79c
minseok <tir2986@naver.com>
2024-03-25 11:09:07 +0900

    feat: make reservation type

--- src/view/InputView.java
+++ src/view/InputView.java
@@ -8,17 +8,4 @@ public class InputView {
     public String readLine() {
         return scanner.nextLine();
     }
-
-    public String readOption(String... options) {
-        String option;
-        while (!isInOptions(option = readLine())) {}
-        return option;
-    }
-
-    private boolean isInOptions(String input, String... options) {
-        for (String opt : options) {
-            if (input.equals(opt)) return true;
-        }
-        return false;
-    }
 }
