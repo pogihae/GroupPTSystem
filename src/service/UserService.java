ff2c0bd46edf7accbbdc7eff44cc40297b9ec34f
minseok <tir2986@naver.com>
2024-03-25 10:31:31 +0900

    feat: make reservation and util

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -9,7 +9,23 @@ import java.util.List;
 
 public class UserService {
 
+    public static User loginedUser = null;
+
+    public static User.Role getLoginedUserRole() {
+        if (loginedUser == null) {
+            throw new IllegalStateException("로그인한 유저가 존재하지 않습니다.");
+        }
+        return loginedUser.getRole();
+    }
+
     private GroupPTRepository repository;
+
+    public void login(User user) {
+        if (loginedUser != null) {
+            throw new IllegalStateException("이미 로그인한 유저가 존재합니다.");
+        }
+        loginedUser = user;
+    }
 
     public void signUp(User user) {
         if (user.getRole().equals(User.Role.TRAINER)) {
